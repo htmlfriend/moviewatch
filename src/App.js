73fe166d518747325b01fb44cfa1e8d2619d15f4
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-25 16:52:52 -0500

    try someting get

--- src/App.js
+++ src/App.js
@@ -1,107 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
 import "./App.css";
-const App = () => {
-  const [news, setNews] = useState([]);
-  const [searchQuery, setSeachQuery] = useState("react");
+import "./Myname";
+import axios from "axios";
+import Movies from "./Movies";
 
-  const [url, setUrl] = useState(
-    "https://hn.algolia.com/api/v1/search?query=react"
-  );
-  const [loading, setLoading] = useState(false);
-
-  const showLoading = () => (loading ? <h2>Loading ... </h2> : "");
-
-  const searchForm = () => (
-    <form onSubmit={hadleSubmit}>
-      <input type='text' value={searchQuery} onChange={searchFormInput} />
-      <button>Search</button>
-    </form>
-  );
-  // fetch news
-  const fetchNews = (url) => {
-    setLoading(true);
-    fetch(url)
-      // .then(data => data.json())
-      // .then(data => console.log(data))
-      .then((result) => result.json())
-      .then((data) => {
-        setNews(data.hits);
-        setLoading(false);
-      })
-      .catch((error) => console.log(error));
-  };
-  useEffect(() => {
-    fetchNews(url);
-  }, [url]);
-
-  const searchFormInput = (e) => {
-    setSeachQuery(e.target.value);
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
   };
 
-  const hadleSubmit = (e) => {
-    e.preventDefault();
-    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({
+      isLoading: false,
+      movies,
+    });
+    console.log(movies);
   };
 
-  const mapArray = () =>
-    news.map((elem, index) => <p key={index}>{elem.title}</p>);
+  componentDidMount() {
+    this.getMovies();
+  }
 
-  return (
-    <div>
-      <h2>News</h2>
-      {showLoading()}
-      {searchForm()}
-      {mapArray()}
-    </div>
-  );
-};
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-
-//   const increment = () => {
-//     setCount(count + 1);
-//   };
-
-//   useEffect(() => {
-//     document.title = `Clicked ${count} times`
-//   })
-
-//   return (
-//     <div>
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//       <button onClick={increment}>Clicked {count} times</button>
-//     </div>
-//   );
-// };
-
-//  class App extends Component {
-//   state ={
-//     count :0
-//   }
-
-//   increment =() => {
-//     this.setState({
-//       count: this.state.count + 1
-//     })
-//   };
-
-//   componentDidMount() {
-//     document.title = `Clicked ${this.state.count} times`
-//   }
-
-//   componentDidUpdate() {
-//     document.title = `Clicked ${this.state.count} times`
-//   }
-//   render() {
-//   return (
-//     <div>
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//       <button onClick={this.increment}>Clicked {this.state.count} times</button>
-//     </div>
-//   );
-// }
-// }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        <section className='container'>
+          {isLoading ? (
+            <div className='loader'>
+              <span className='loader__text'>Loading ...</span>
+            </div>
+          ) : (
+            <div className='movies'>
+              {movies.map((movie) => (
+                <Movies key={movie.id} movie={movie} />
+              ))}
+            </div>
+          )}
+        </section>
+      </div>
+    );
+  }
+}
 export default App;

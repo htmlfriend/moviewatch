b966d6fee70e0f5451b92c74780d93822c093287
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-25 20:27:19 -0500

    Updates

---
+++ precache-manifest.20d18803a1ce0f0959a05edaf6b21ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9667485b096411fae84c",
+    "url": "/moviewatch/static/css/main.4b0594f0.chunk.css"
+  },
+  {
+    "revision": "9667485b096411fae84c",
+    "url": "/moviewatch/static/js/main.9667485b.chunk.js"
+  },
+  {
+    "revision": "1e972fb5a6b99be5d235",
+    "url": "/moviewatch/static/js/1.1e972fb5.chunk.js"
+  },
+  {
+    "revision": "0b78a1a1d1ca5a46166f",
+    "url": "/moviewatch/static/js/runtime~main.0b78a1a1.js"
+  },
+  {
+    "revision": "0efeb889b928dbdb0d236511104d819d",
+    "url": "/moviewatch/index.html"
+  }
+];
